d60651f75dff66e349316ed4798b841f87fc6996
Jason Brooks <JasonBrooksBio@gmail.com>
2019-05-20 14:12:58 -0500

    Functioning practice model with Router/Link and Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom"
 import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom"
 import Kennel from "./components/Kennel"
 
-ReactDOM.render(<Kennel />, document.getElementById('root'));
+import "./index.css"
+
+
+
+ReactDOM.render(<Router><Kennel /></Router>, document.getElementById('root'));
 
 
